56d1539b1513745736222bcb1592522f6e7d5f38
Andrew Miracle <me@andrewmiracle.com>
2020-07-16 10:38:50 +0000

    disable transpiling to js for all modules

--- imports/ui/pages/auth/index.jsx
+++
@@ -1,6 +0,0 @@
-import Login from './Login';
-import Signup from './Signup';
-import ResetPassword from './Reset';
-import Logout from './Logout';
-// Export pages from root
-export { Login, Signup, ResetPassword, Logout };
